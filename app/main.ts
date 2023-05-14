b0c477083c7475de5fac4f53f8ef33d11d29408b
vogel <over.castrillon22396@ucaldas.edu.co>
2023-05-14 10:21:22 -0500

    Order of variables is now correct

--- app/main.ts
+++ app/main.ts
@@ -5,8 +5,8 @@ console.log(salute)
 // Inputs & outputs //
 console.log(process.argv)
 
-const a: number = Number(process.argv[3])
-const b: number = Number(process.argv[2])
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
 
 console.log(`Sum result of ${a} and ${b} is ${a+b}.`)
 

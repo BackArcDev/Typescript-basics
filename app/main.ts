600ca9700355f20c0f6389f41c342ad4a08348e2
vogel <over.castrillon22396@ucaldas.edu.co>
2023-05-14 13:15:10 -0500

    Addition of parsing types

--- app/main.ts
+++
@@ -1,5 +0,0 @@
-let bool = true || false
-
-if (bool) {
-    console.log(`It's true`)
-} 
---
+++ app/types.ts
@@ -0,0 +1,14 @@
+// Booleano
+let esVerdadero: boolean = true;
+
+// Número
+let entero: number = 6;
+let decimal: number = 6.0;
+let hexadecimal: number = 0xf00d;
+let binario: number = 0b1010;
+let octal: number = 0o744;
+console.log(octal)
+
+// Cadena de texto
+let color: string = "azul";
+color = 'rojo';

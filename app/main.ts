d030226fedccb118452e34439f21694ffec03e70
vogel <over.castrillon22396@ucaldas.edu.co>
2023-05-14 09:56:57 -0500

    First commit

---
+++ app/main.ts
@@ -0,0 +1,11 @@
+// Start //
+console.log('Hi, math!')
+
+// Inputs & outputs //
+console.log(process.argv)
+
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
+
+console.log(`Sum result of ${a} and ${b} is ${a-b}.`)
+

d992591e2b33d6baf0d501ab78759c3244114f2a
vogel <over.castrillon22396@ucaldas.edu.co>
2023-05-14 10:11:57 -0500

    Correction of substraction to sum

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,12 @@
-// Start //
-console.log('Hi, math!')
+// Printing into console //
+const salute: string = 'Hi, math!'
+console.log(salute)
 
 // Inputs & outputs //
 console.log(process.argv)
 
-const a: number = Number(process.argv[2])
-const b: number = Number(process.argv[3])
+const a: number = Number(process.argv[3])
+const b: number = Number(process.argv[2])
 
-console.log(`Sum result of ${a} and ${b} is ${a-b}.`)
+console.log(`Sum result of ${a} and ${b} is ${a+b}.`)
 
